daeba2c507473dbab91afa4baf7fdadb704b57e8
me56 <uoa163@163.com>
2019-11-09 17:36:48 +0800

    adjust js:active

--- js/common.js
+++ js/common.js
@@ -1,5 +1,4 @@
 window.onload = function () {
-    localStorage.removeItem('index');
     let ul = document.getElementById('ul');
     let lis = ul.getElementsByTagName('li');
     let i = 0, j = 0, origin_class = '';
