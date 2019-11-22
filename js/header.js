8df387e6d45d30fda949730bce31ae9ea96ca796
uoa <uoa163@163.com>
2019-11-22 17:59:27 +0800

    adjust layout:header

--- js/header.js
+++ js/header.js
@@ -1,19 +1,8 @@
-let device_type = navigator.userAgent;//获取userAgent信息
-let os = '';
-if (device_type.search('.NET') != -1) {
-    os = 1
-} else if (device_type.search('iPhone') != -1) {
-    os = 1;
-} else if (device_type.search('Edge') != -1) {
-    os = 1;
-} else {
-    os = 2
-}
 
-if (os === 1) {//ios系统的处理
+if (head.import == undefined) {
     $.get('/header.html', function (data) {
         $('body').prepend(data);
     })
-} else {
+}else{
     document.write(head.import.body.innerHTML);
 }
