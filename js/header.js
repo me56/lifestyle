4418eacf60c16f00134fdb5a969a2bc410ca21b0
uoa <uoa163@163.com>
2019-11-22 17:24:23 +0800

    adjust layout:header

--- js/header.js
+++ js/header.js
@@ -1,7 +1,16 @@
 let device_type = navigator.userAgent;//获取userAgent信息
-let md = new MobileDetect(device_type);//实例化mobile-detect
-let os = md.os();//获取系统
-if (os == "iOS") {//ios系统的处理
+let os = '';
+if (device_type.search('.NET') != -1) {
+    os = 1
+} else if (device_type.search('iPhone') != -1) {
+    os = 1;
+} else if (device_type.search('Edge') != -1) {
+    os = 1;
+} else {
+    os = 2
+}
+
+if (os === 1) {//ios系统的处理
     $.get('/header.html', function (data) {
         $('body').prepend(data);
     })
